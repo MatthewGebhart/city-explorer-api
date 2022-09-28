9a2ce61a0745a42de575d8f99fc039ae5689c3d6
Matthew Gebhart <Matthewgebhart@gmail.com>
2022-09-27 23:26:11 -0700

    initial commit, not working yet

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # city-explorer-api
 Back end for City-Explorer lab
+
+
+**Author**: Matthew Gebhart
+**Version**: 1.0.1 (increment the patch/fix version number if you make more commits past your first submission)
+
+## Overview
+getting practice using express server and sending requests back and forth
+
+## Getting Started
+--- outside of the score of scope of this project
+
+## Architecture
+express server, node, 
+
+## Change Log
+9-27-2022
+implemented server and proof of life
+
+## Credit and Collaborations
+Julian Gallegos 
---
+++ server.js
@@ -0,0 +1,57 @@
+'use strict'
+//This will be our server
+
+//Set Up:
+// ----------
+
+require('dotenv').config();
+//express server
+const express = require('express');
+// allows for Cross Origin Resource Sharing
+const cors = require('cors');
+// load data
+const data = require('./data/weather.json');
+//start our server
+const app = express();
+
+//Middleware
+app.use(cors());
+
+//Declare our PORT variable
+const PORT = process.env.PORT || 3001;
+
+// Listening for connection
+app.listen(PORT, () => console.log(`listening on Port ${PORT}`));
+
+// Endpoints:
+// ------------------
+
+app.get('/weather', (req, res) => {
+    console.log(req.query);
+    let place =data.find( data => data.city_name.toLowerCase() === req.query.searchQuery.toLowerCase());
+    try { 
+        res.send(weatherData(place.data));
+    } catch {
+        place = 'Error, we don\'t have data for that location';
+    }
+});
+
+
+function weatherData(arr) {
+    return arr.map(data) => {
+        return new Forecast(data.datetime, data.weather.description)
+    });
+};
+
+class Forecast {
+    constructor(date, description){
+        this.date = date,
+        this.description = description;
+    }
+};
+
+// Catch all endpoint:
+
+app.get('*', (req, res) => {
+    res.status(404).send('uh oh, Ya Done 404');
+})
